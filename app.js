99f5d6d33e79829ae90429216983efd164dc4382
Carl-Gustaf <ewerbring@gmail.com>
2019-07-11 18:28:26 +0200

    heroku

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const MongoStore = require("connect-mongo")(session);
 const flash = require("connect-flash");
 
 mongoose
-  .connect("mongodb://localhost/visionsbilder", { useNewUrlParser: true })
+  .connect(process.env.MONGODB_URI || "mongodb://localhost/visionsbilder", {
+    useNewUrlParser: true
+  })
   .then(x => {
     console.log(
       `Connected to Mongo! Database name: "${x.connections[0].name}"`
